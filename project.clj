b6b68b8af5d17b35ef187a38c439da5db5770228
Christoph Frick <cf@ofnir.net>
2018-12-18 23:04:13 +0100

    Update versions

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject net.ofnir/wrepl.relative-clj-http "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [integrant "0.6.3"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [integrant "0.7.0"]
                  [clj-http "3.9.1"]
-                 [cheshire "5.8.0"]
+                 [cheshire "5.8.1"]
                  [org.clojure/data.xml "0.0.8"]
                  [net.ofnir/relative-clj-http "0.1.1"]])

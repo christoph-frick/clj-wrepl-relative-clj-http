d4b79341913eb4600d399bcbbd452f3a175ef013
Christoph Frick <cf@ofnir.net>
2020-01-04 14:01:15 +0100

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.ofnir/wrepl.relative-clj-http "0.1.0"
+(defproject net.ofnir/wrepl.relative-clj-http "0.1.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [integrant "0.7.0"]
                  [clj-http "3.10.0"]

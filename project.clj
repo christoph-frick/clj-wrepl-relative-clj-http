3587efb72e03810388492987a9a98338f679170e
Christoph Frick <cf@ofnir.net>
2020-02-11 12:32:30 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject net.ofnir/wrepl.relative-clj-http "0.1.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [integrant "0.7.0"]
+                 [integrant "0.8.0"]
                  [clj-http "3.10.0"]
                  [net.ofnir/relative-clj-http "0.1.3"]
                  [net.ofnir/clj-http-request "0.1.0"]]
